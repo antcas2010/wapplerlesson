5d65720ca9b97ac4bdd4723b1a313b3ac79797d5
antcas2010 <antcas2010@gmail.com>
2022-11-12 20:53:48 +0200

    First commitment_registration_db

---
+++ .wappler/migrations/db/20221105203503_creae_initial_user_roles.js
@@ -0,0 +1,23 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('user', async function (table) {
+      table.increments('iuser_d');
+      table.datetime('date_signup').defaultTo(knex.fn.now());
+      table.string('password');
+      table.string('email');
+      table.string('first_name');
+      table.string('last_name');
+    })
+    .createTable('role', async function (table) {
+      table.increments('role_id');
+      table.string('user_id');
+      table.string('role', 1);
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTable('role')
+    .dropTable('user')
+};
---
+++ .wappler/migrations/db/20221110194210_authorization_changes.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .table('user', async function (table) {
+      table.string('authcode');
+      table.datetime('validated');
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .table('user', async function (table) {
+      table.dropColumn('authcode');
+      table.dropColumn('validated');
+    })
+};
